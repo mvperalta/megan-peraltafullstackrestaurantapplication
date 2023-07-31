39f84a1d5d94f4a064110773d4c4dda1cc2ab536
Megan Peralta <meganperalta@Megans-MacBook-Pro.local>
2023-07-31 16:36:48 -0400

    Adding frontend directory

---
+++ frontend/next.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  reactStrictMode: true,
+  images: {
+    domains: ["localhost"],
+    loader: "default",
+    path: "/_next/image",
+    disableStaticImages: true,
+    deviceSizes: [320, 420, 768, 1024, 1200], // Modify these sizes according to your requirements
+  },
+};
+
+
