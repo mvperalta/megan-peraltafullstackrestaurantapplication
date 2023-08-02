e8b9de6f91613040539251b1bb2ac32fe7c872fa
Megan Peralta <meganperalta@Megans-MacBook-Pro.local>
2023-08-02 12:03:31 -0400

    Updated database config

--- backend/config/database.js
+++ backend/config/database.js
@@ -4,9 +4,9 @@ module.exports = ({ env }) => ({
     connection: {
       host: env('DATABASE_HOST', '127.0.0.1'),
       port: env.int('DATABASE_PORT', 5432),
-      database: env('DATABASE_NAME', 'nextapp'),
-      user: env('DATABASE_USERNAME', 'postgres'),
-      password: env('DATABASE_PASSWORD', 'Liason26'),
+      database: env('DATABASE_NAME', 'nextapp_okyp'),
+      user: env('DATABASE_USERNAME', 'myapp'),
+      password: env('DATABASE_PASSWORD', 'Lg2B2Si8OrliMR1CxvosjyiT6kmPhgRoE'),
       ssl: env.bool('DATABASE_SSL', false),
     },
   },
