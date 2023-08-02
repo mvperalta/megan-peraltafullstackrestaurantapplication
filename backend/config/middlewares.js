b1d22bcef59ef3ce1bfb2d57a7ca9a2759b5edb3
Megan Peralta <meganperalta@Megans-MacBook-Pro.local>
2023-08-02 13:44:23 -0400

    Updated middleware for CSP configuration

--- backend/config/middlewares.js
+++ backend/config/middlewares.js
@@ -8,6 +8,20 @@ module.exports = [
       origin: ['https://meganperalta-fullstackrestaurantapp-kr7t2uvup-mvperalta.vercel.app'],
     },
   },
+  // Add the CSP middleware with proper configuration
+  {
+    name: 'helmet',
+    enabled: true,
+    config: {
+      contentSecurityPolicy: {
+        directives: {
+          // Update the style-src directive to allow CSS from the specified origin
+          'style-src': ["'self'", 'https://meganperalta-fullstackrestaurantapp-kr7t2uvup-mvperalta.vercel.app'],
+          // Add any other directives as needed for your application
+        },
+      },
+    },
+  },
   'strapi::poweredBy',
   'strapi::logger',
   'strapi::query',
