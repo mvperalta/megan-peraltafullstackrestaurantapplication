3451817e51cc2df86aa501f3a04eeffa5e03ee53
Megan Peralta <meganperalta@Megans-MacBook-Pro.local>
2023-08-02 16:33:50 -0400

    Fixed middleware

--- backend/config/middlewares.js
+++ backend/config/middlewares.js
@@ -1,27 +1,7 @@
 module.exports = [
   'strapi::errors',
   'strapi::security',
-  {
-    name: 'strapi::cors',
-    enabled: true,
-    params: {
-      origin: ['https://meganperalta-fullstackrestaurantapp-kr7t2uvup-mvperalta.vercel.app'],
-    },
-  },
-  // Add the CSP middleware with proper configuration
-  {
-    name: 'helmet',
-    enabled: true,
-    config: {
-      contentSecurityPolicy: {
-        directives: {
-          // Update the style-src directive to allow CSS from the specified origin
-          'style-src': ["'self'", 'https://meganperalta-fullstackrestaurantapp-kr7t2uvup-mvperalta.vercel.app'],
-          // Add any other directives as needed for your application
-        },
-      },
-    },
-  },
+  'strapi::cors',
   'strapi::poweredBy',
   'strapi::logger',
   'strapi::query',
