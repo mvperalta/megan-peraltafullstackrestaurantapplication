374dc5c575c52a3f14f75ea012e31871d455d7fd
Megan Peralta <meganperalta@Megans-MacBook-Pro.local>
2023-08-04 16:19:22 -0400

    changed middleware.js

--- backend/config/middlewares.js
+++ backend/config/middlewares.js
@@ -1,5 +1,6 @@
 module.exports = [
   'strapi::errors',
+  'strapi::security',
   'strapi::cors',
   'strapi::poweredBy',
   'strapi::logger',
@@ -8,30 +9,4 @@ module.exports = [
   'strapi::session',
   'strapi::favicon',
   'strapi::public',
-  {
-    name: 'strapi::security',
-    config: {
-      contentSecurityPolicy: {
-        useDefaults: true,
-        directives: {
-          'connect-src': ["'self'", 'https:'],
-          'img-src': [
-            "'self'",
-            'data:',
-            'blob:',
-            'market-assets.strapi.io',
-            'render-strapi-backend.s3.us-east-2.amazonaws.com',
-          ],
-          'media-src': [
-            "'self'",
-            'data:',
-            'blob:',
-            'market-assets.strapi.io',
-            'render-strapi-backend.s3.us-east-2.amazonaws.com',
-          ],
-          upgradeInsecureRequests: null,
-        },
-      },
-    },
-  },
 ];
