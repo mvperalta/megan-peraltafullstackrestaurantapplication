910621b4152ddbd7a9c459ccf66dd09121112431
Megan Peralta <meganperalta@Megans-MacBook-Pro.local>
2023-08-04 16:43:49 -0400

    changed frontend urls

--- frontend/components/RestaurantList.jsx
+++ frontend/components/RestaurantList.jsx
@@ -32,8 +32,8 @@ function RestaurantCard({ data }) {
           className="w-full rounded-2xl"
           height={300}
           width={300}
-          src={`https://strapi-dr7e.onrender.com${
-            data?.attributes?.image?.data?.[0]?.attributes?.url || ""
+          src={`${process.env.STRAPI_URL || "https://strapi-dr7e.onrender.com"}${
+          data.attributes.image.data [0].attributes.url
           }`}
           alt=""
         />
