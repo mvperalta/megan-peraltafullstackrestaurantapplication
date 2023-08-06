ab45ef132d8f66cee142dc32b2d11ab548d2d687
Megan Peralta <meganperalta@Megans-MacBook-Pro.local>
2023-08-06 11:53:26 -0400

    update image domains in next.config

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,12 +1,10 @@
 module.exports = {
   reactStrictMode: true,
   images: {
-    domains: ["localhost", "https://strapi-drr3.onrender.com/", "myrestaurantimagesbucket.s3.amazonaws.com"],
+    domains: ["localhost", "strapi-drr3.onrender.com", "myrestaurantimagesbucket.s3.amazonaws.com"],
     loader: "default",
     path: "/_next/image",
     disableStaticImages: true,
     deviceSizes: [320, 420, 768, 1024, 1200], // Modify these sizes according to your requirements
   },
 };
-
-
